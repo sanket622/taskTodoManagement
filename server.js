29ce125a219ba33a66ba42003361d91beb888600
sanket622 <sanketkumar0068@gmail.com>
2025-03-09 12:32:10 +0530

    update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
@@ -21,6 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(limiter);
+app.use(cors())
 
 
 app.use('/api/users', authRoutes);
